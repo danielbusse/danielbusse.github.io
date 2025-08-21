4fae7191b301d04437ecbc104ba98f4cfa8b29dd
danielbusse <daniel.busse1997@gmail.com>
2025-08-21 20:00:12 +0200

    changed pdf viewer back to simpler object/embed

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 import EditNote from '@/components/EditNote';
 
-import styles from './cv.module.scss'
+import styles from './cv.module.scss';
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -17,7 +17,7 @@ export default function ProjectsPage() {
         <div className={styles.main}>
             <EditNote />
             <div className={styles.cv}>
-                <PDFViewer file="/pdfs/cv_eng.pdf" page={1}/>
+                <PDFViewer file="/pdfs/cv_eng.pdf"/>
             </div>
         </div>
     );
--- components/PDFViewer.tsx
+++ components/PDFViewer.tsx
@@ -1,27 +1,18 @@
 "use client"
 
-import { pdfjs, Document, Page } from 'react-pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import styles from './pdf.module.scss';
 
-import './pdf.module.scss';
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
 
 type PDFViewerProps = {
     file: string;
-    page?: number;
 }
 
-export default function PDFViewer({file, page=0}: PDFViewerProps) {
+export default function PDFViewer({file}: PDFViewerProps) {
     return(
-        <div className="pdf">
-            <Document file={file}>
-                <Page pageNumber={page} width={500}/>
-            </Document>
+        <div className={styles.wrapper}>
+            <object data={file} type="application/pdf">
+                <embed src={file} type="application/pdf"/>
+            </object>
         </div>
     );
 }
