bc3e6b5311f79667dd9026ea68cf91e4b58cc2f5
danielbusse <daniel.busse1997@gmail.com>
2026-01-06 17:44:36 +0100

    added test timeline

---
+++ app/timeline_test/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WheelTimeline from './WheelTimeline';
+
+export default function TimelineTestPage() {
+    return (
+        <main style={{ background: '#111', minHeight: '100vh', color: '#fff' }}>
+            <WheelTimeline />
+        </main>
+    );
+}
